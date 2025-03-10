08a2bb8ef5006f86888b22aee27ce493f07906ce
shamshad ahamad <shamshad22499@gmail.com>
2025-03-10 13:53:24 +0530

    change server data

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import DataBaseConntect from './DataBaseToConnect/DbConnect.js';
-import router from './Router/allRouter.js';
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import DataBaseConntect from "./DataBaseToConnect/DbConnect.js";
+import router from "./Router/allRouter.js";
+import cors from "cors";
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin : "https://employee-front-end-gold.vercel.app",
-  credentials : true,
-}));
-
+app.use(
+  cors({
+    origin: "https://employee-front-end-gold.vercel.app",
+    credentials: true,
+  })
+);
 
 const port = process.env.PORT || 5000;
 const url = process.env.URL || "https://employee-backend-last.vercel.app";
 app.use(express.urlencoded({ extended: true }));
 
-//for using images at frontEnd 
-app.use(express.static("public/uploads"))
+//for using images at frontEnd
+app.use(express.static("public/uploads"));
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server');
-}
-);
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    data: [{ name: "shamshad Ahamad", education: "higher", dob: "20/04/2002" }],
+    error: false,
+    message: "server is running smoothly on the web",
+  });
+});
 
-app.use("/api" , router)
+app.use("/api", router);
 
-DataBaseConntect().then(()=>{
+DataBaseConntect().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on ${url}:${port}`);
   });
-})
+});
